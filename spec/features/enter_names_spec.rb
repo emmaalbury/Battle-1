9c1bdb5bfcbeda9bbfb7e5f0fae23277377068ba
Makers Academy <makersacademy@Makerss-MacBook-Pro.local>
2018-09-19 16:00:07 +0100

    created enter_names test

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,11 @@
+feature 'Enter Names' do
+  scenario "Submits a name in a form" do
+    visit '/'
+
+    fill_in :name_1, with: "Emma"
+    fill_in :name_2, with: "Florence"
+    click_button "Submit"
+
+    expect(page).to have_content ("Emma vs. Florence")
+  end
+end
